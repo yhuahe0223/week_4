3b5132ac7834fa51506f4046b3d1df1d4f9fa4b1
He <yhuahe@cps.k12.il.us>
2024-09-20 15:05:18 -0500

    mini_challenge

---
+++ mini_challengeWK4.py
@@ -0,0 +1,127 @@
+# String Properties Practice #1
+# Concatenate the text "Repetition" 15 times and display the result on the screen.
+# Luckily, you know that strings are multipliable and you can do this activity in a simple and elegant way.
+# result= "repetition "*1
+# print(result)
+
+# first_paragraph = "When, in the course of human events, it becomes necessary for one people to dissolve the political bands which have connected them with another, and to assume, among the powers of the earth, the separate and equal station to which the laws of nature and of nature's God entitle them, a decent respect to the opinions of mankind requires that they should declare the causes which impel them to the separation."
+# first_paragraph.replace("people","citizens")
+# print(first_paragraph)
+# new_first_paragraph= ",".join(first_paragraph)
+# print(new_first_paragraph)
+
+
+# String Properties Practice #2
+# Check if the word "beach" is not found in the following haiku. You should print the boolean.
+sentence1= "Whitecaps on the bay A broken signboard banging In the April wind."
+# — Richard Wright, collected in Haiku: This Other World, 1998
+beach_in = False 
+print(sentence1 ,"is beach in the haiku?", beach_in)
+
+
+
+
+# String Properties Practice #3
+# Check the Python Documentation to find the description of the len() function. Then, display on the screen the length (in number of characters) of the word electroencephalographist.
+
+################################list###################################################
+# Lists Practice #1
+# Print the following list on the screen:
+# ["Python", "is", "easy", "to", "learn"]
+
+# Lists Practice #1
+# Create a list with 5 elements, inside the variable my_list. You can include strings, booleans, numbers, etc.
+
+# Lists Practice #2
+# Add the element "motorcycle" to the following list of means of transportation:
+# transportation_means = ["plane", "car", "ship", "bicycle"]
+# You must not modify the already supplied line of code, but must use the appropriate list method to add a new element.
+
+# Lists Practice #3
+# Use the pop() method to remove the third item from the following list called fruits, and store it in a variable called deleted_item. Use list methods without altering the line of code already supplied.
+# apple
+# banana
+# mango
+# cherry
+# watermelon
+
+#######################################Dictionaries###############################
+# Dictionaries Practice #1
+# Create a dictionary called fruits with the following key-value pairs:
+# apple --> red
+# banana --> yellow
+# mango --> green
+# cherry --> red
+# watermelon --> green
+# Display the dictionary on the screen.
+
+# Dictionaries Practice #1
+# Create a dictionary called my_dict that stores the following information about a person:
+# name: Karen
+# surname: Jurgens
+# age: 35
+# occupation: Journalist
+# The names of the keys and values must be equal to the ones indicated above.
+
+#   Dictionaries Practice #2
+# Use print to returns the second item of the list called points2, inside the following dictionary.
+# If the value 300 were to change in the future, the code should work the same to return the value at that same position. To do this, you must refer to the names of the keys and/or indexes as appropriate.
+# my_dict = {"values_1":{"v1":3,"v2":6},"points":{"points1":9,"points2":[10,300,15]}}
+# print(my_dict[]) #Use dictionary indices to extract the second item of points2
+
+# Dictionaries Practice #3
+# Update the information in our dictionary called my_dict (reassigning new values to the keys as appropriate), and add a new key called "country" (without a tilde). The new data is:
+# name: Karen
+# surname: Jurgens
+# age: 36
+# occupation: Editor
+# country: Colombia
+# to do this, you should not change the line of code already written, but update the values using dictionary methods.
+# my_dict = {"name":"Karen", "surname":"Jurgens", "age":35, "occupation":"Journalist"}
+
+################################Tuples##################################
+
+# Tuples Practice #1
+# Use a tuple method to count the number of times the value 2 appears in the following tuple, and display the result (integer) on the screen:
+
+# my_tuple = (1, 2, 3, 2, 3, 1, 3, 2, 3, 3, 3, 1, 3, 2, 2, 1, 3, 2)
+
+# Tuples Practice #2
+# Convert the following tuple to a list, and store it in a variable called my_list.
+
+# my_tuple = (1, 2, 3, 2, 3, 1, 3, 2)
+
+# Tuples Practice #3
+# Extract the elements of the following tuple into four variables: a, b, c, d
+
+# my_tuple = (1, 2, 3, 4)
+
+###############################SETS########################################
+# Sets Practice #1
+# Join the following sets into one, called my_set_3:
+# {1, 2, "three", "four"}
+# {"three", 4, 5}
+# my_set_1 = {1, 2, "three", "four"}
+
+# my_set_2 = {"three", 4, 5}
+
+# Sets Practice #2
+# Remove a random item from the following set, using set methods.
+# raffle = {"Rachel", "Monica", "Phoebe", "Joey", "Chandler", "Ross"}
+
+# Sets Practice #3
+# Add the name Gunther to the following set, using set methods:
+# raffle = {"Rachel", "Monica", "Phoebe", "Joey", "Chandler", "Ross"}
+
+###########################Booleans#######################################
+
+# Booleans Practice #1
+# Make a comparison that returns a boolean and store the result (True/False) in a variable called test
+
+# Booleans Practice #2
+# Check if 17834/34 is greater than 87*56 and print the boolean result to the screen using print()
+
+# Booleans Practice #3
+# Check if the square root of 25 is equal to 5 and display the result (boolean) on the screen using print()
+
+###############################Proceed to last slide#################################
